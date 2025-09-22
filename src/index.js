c086c092d6a371f3e5f6087de05bb76e6fcd1b18
MineYarick <89548578+MineYarick@users.noreply.github.com>
2025-09-22 22:55:40 +0300

    Liquor store

--- src/index.js
+++ src/index.js
@@ -1,39 +1,15 @@
-//const firstName: string = "Jonnie";
-//const lastName: string = "Ivsnov";
-//const pi: number = 3.14;
-//console.log(`Hello, ${firstName} ${lastName}!`);
-//console.log(`The value of pi is ${pi}.`);
-// Вивести 10 консоль.логів у консоль
-//for (let i = 0; i < 10; i++){
-//    console.log(`Число з циклу for - ${i}`);
-//}
-// Вивести ліьки парні числа
-//for (let i = 0; i < 10; i += 2){
-//    console.log(`Число з циклу for - ${i}`);
-//}
-//for (let i = 0; i < 10; i++){
-//    if (i % 2 === 0){
-//        console.log(`Число з циклу for - ${i}`);
-//    }
-//}
-//console.log(this.document);
-// Задача: створити невеликий додаток, який може обчислити факторіал числа.
-// Факторіал числа - добуток всіх цілих чисел від 1 до цього числа включно.
-// 5! = 1 * 2 * 3 * 4 * 5 = 120
-const form = document.querySelector('#factorial-form');
-const stepsList = document.querySelector('#factorial-steps');
+const form = document.querySelector('#alcohol-form');
+const messageDiv = document.querySelector('#message');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    stepsList.textContent = '';
-    // Отримати введене число
-    const numberInput = document.querySelector('#number');
-    const number = Number(numberInput.value);
-    // Обчислюємо факторіал та додавання кожної ітерації до список
-    let factorial = 1;
-    for (let i = 1; i <= number; i++) {
-        factorial *= i;
-        const listItem = document.createElement('li');
-        listItem.textContent = `${factorial}`;
-        stepsList.appendChild(listItem);
+    const nameInput = document.querySelector('#name');
+    const ageInput = document.querySelector('#age');
+    const name = nameInput.value;
+    const age = Number(ageInput.value);
+    if (age >= 18) {
+        messageDiv.textContent = `${name}, Вам дозволено купувати алкоголь.`;
+    }
+    else {
+        messageDiv.textContent = `${name}, Вам заборонено купувати алкоголь.`;
     }
 });
--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,19 @@
-//const firstName: string = "Jonnie";
-//const lastName: string = "Ivsnov";
-
-//const pi: number = 3.14;
-
-//console.log(`Hello, ${firstName} ${lastName}!`);
-//console.log(`The value of pi is ${pi}.`);
-
-// Вивести 10 консоль.логів у консоль
-
-//for (let i = 0; i < 10; i++){
-//    console.log(`Число з циклу for - ${i}`);
-//}
-
-// Вивести ліьки парні числа
-
-//for (let i = 0; i < 10; i += 2){
-//    console.log(`Число з циклу for - ${i}`);
-//}
-
-//for (let i = 0; i < 10; i++){
-//    if (i % 2 === 0){
-//        console.log(`Число з циклу for - ${i}`);
-//    }
-//}
-
-//console.log(this.document);
-
-// Задача: створити невеликий додаток, який може обчислити факторіал числа.
-// Факторіал числа - добуток всіх цілих чисел від 1 до цього числа включно.
-// 5! = 1 * 2 * 3 * 4 * 5 = 120
-const form = document.querySelector('#factorial-form') as HTMLFormElement;
-const stepsList = document.querySelector('#factorial-steps') as HTMLOListElement;
+const form = document.querySelector('#alcohol-form') as HTMLFormElement;
+const messageDiv = document.querySelector('#message') as HTMLDivElement;
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    stepsList.textContent = '';
-    // Отримати введене число
-    const numberInput = document.querySelector('#number') as HTMLInputElement;
-    const number: number = Number(numberInput.value);
 
-    // Обчислюємо факторіал та додавання кожної ітерації до список
-    let factorial = 1;
-    for (let i = 1; i <= number; i++) {
-        factorial *= i;
+    const nameInput = document.querySelector('#name') as HTMLInputElement;
+    const ageInput = document.querySelector('#age') as HTMLInputElement;
+    
+    const name: string = nameInput.value;
+    const age: number = Number(ageInput.value);
 
-        const listItem: HTMLLIElement = document.createElement('li');
-        listItem.textContent = `${factorial}`;
-        stepsList.appendChild(listItem);
+    if (age >= 18) {
+        messageDiv.textContent = `${name}, Вам дозволено купувати алкоголь.`;
+    } else {
+        messageDiv.textContent = `${name}, Вам заборонено купувати алкоголь.`;
     }
 });
+
